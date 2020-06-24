7ae129ba86fc9deb1bb7a1ed8bec5fcf46a4ff30
Harrison <mac2015@mac2015s-MBP.fios-router.home>
2020-06-23 23:08:23 -0400

    add navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Navbar() {
-  const icon = (iconName) => {
-    return <FontAwesomeIcon icon={iconName} />;
+  const icon = (iconName, className) => {
+    return <FontAwesomeIcon icon={iconName} className={className} />;
   };
 
+  const baseClass = "nav-bar";
+
   return (
-    <div className="nav-bar">
-      <h1>Peekskill, NY</h1>
-      {icon("chevron-down")}
-      {icon("search")}
+    <div className={baseClass}>
+      <div className={`${baseClass}-location`}>
+        <h1 className={`${baseClass}-heading`}>Peekskill, NY</h1>
+        {icon("chevron-down", `${baseClass}-location-selector-icon`)}
+      </div>
+      {icon("search", `${baseClass}-search-icon`)}
     </div>
   );
 }
