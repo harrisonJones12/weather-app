dea2fc41e6bc894d837c407eb8ae78eb56ccd9ed
Harrison <mac2015@mac2015s-MBP.fios-router.home>
2020-08-25 15:26:44 -0400

    remove Home component/stylesheet, add styles to app.scss, fix UI flaws / ensure 5 day forecast stays at the bottom of page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "icons/fontawesome";
 
-import Home from "components/Home";
+import Navbar from "components/Navbar/Navbar";
+import NextDay from "components/NextDay/NextDay";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <NextDay />
     </div>
   );
 }
